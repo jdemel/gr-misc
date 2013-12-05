72563bd43cfa04fcace921c0f816a48c44e5f83f
Johannes Demel <johannes.demel@ettus.com>
2013-12-04 16:19:37 -0800

    updated copyright notice

--- lib/music_doa_impl.cc
+++ lib/music_doa_impl.cc
@@ -1,6 +1,6 @@
 //* -*- c++ -*- */
 /* 
- * Copyright 2013 Johannes Demel.
+ * Copyright 2013 Ettus Research LLC
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
